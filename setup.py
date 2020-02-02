74533ec0f0b277f39d91f15ea8d255e705fe19b9
raynardj <raynard@rasenn.com>
2020-02-02 22:18:30 +0800

    setup and bin file

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+from setuptools import find_packages
+
+setup(
+    name="doctour",
+    version="0.1.0",
+    author="raynardj",
+    author_email="raynard@rasenn.com",
+    description="A web UI for detailed python coding",
+    packages = find_packages(),
+    include_package_data=True,
+    py_modules=['doctour',],
+    scripts = ['doctour/bin', ],
+    package_data={'doctour':['./doctour/templates/*','./doctour/static/*']},
+    install_requires = [
+        "flask",
+        "flask_appbuilder",
+    ],
+)
